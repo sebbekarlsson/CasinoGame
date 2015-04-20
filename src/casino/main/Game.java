3409b6713ebd2ee383ce4c9f01d0af7f39c130ab
Sebastian Karlsson <sebastian.karlsson@duva.se>
2015-04-20 08:52:47 +0200

    Creates Display

--- src/casino/main/Game.java
+++ src/casino/main/Game.java
@@ -2,9 +2,34 @@ package casino.main;
 
 import java.awt.Dimension;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Game {
 	public static int width = 640;
 	public static int height = width / 16 * 9;
 	public static int scale = 2;
 	public static Dimension FRAMESIZE = new Dimension(width * scale, height * scale);
+	
+	public static void main(String[] args){
+		new Game();
+	}
+	
+	public Game(){
+		try {
+			Display.setDisplayMode(new DisplayMode(FRAMESIZE.width, FRAMESIZE.height));
+			Display.setTitle("Casino Game");
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()){
+			
+		}
+		
+		System.exit(0);
+	}
 }
