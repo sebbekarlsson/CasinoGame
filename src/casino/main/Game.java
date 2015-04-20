02293a9dc6cc8dd8bebddc9a92a993bdd784e34c
Sebastian Karlsson <sebastian.karlsson@duva.se>
2015-04-20 08:49:42 +0200

    Git

--- src/casino/main/Game.java
+++ src/casino/main/Game.java
@@ -1,6 +1,10 @@
 package casino.main;
 
+import java.awt.Dimension;
+
 public class Game {
 	public static int width = 640;
-	public static int height = 480;
+	public static int height = width / 16 * 9;
+	public static int scale = 2;
+	public static Dimension FRAMESIZE = new Dimension(width * scale, height * scale);
 }
