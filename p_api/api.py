0118bbbe1595ab562d57526fc73e2ab5c5c6dfed
phille97 <phille97@users.noreply.github.com>
2015-04-20 10:57:10 +0200

    Keep working on api

--- p_api/api.py
+++ p_api/api.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, jsonify, session, request
+from flask_oauthlib.provider import OAuth2Provider
+
 app = Flask(__name__)
+oauth = OAuth2Provider(app)
 
 @app.route('/')
 def hello_world():
     return 'The API is running!'
 
 if __name__ == '__main__':
+    oauth.init_app(app)
     app.run()
--- p_api/mac_setup.sh
+++ p_api/mac_setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]; then
-	echo "This script must be run as root"
+if [ "$(whoami)" == "root" ]; then
+	echo "Don't run this as root"
 	exit 1
 fi
-
-pip install virtualenv
 
 cd flask
 virtualenv venv --distribute
